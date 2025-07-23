06c9b9d547e95c42ff6f25b451d18c5dc8b53034
Ilya Buziuk <ibuziuk@redhat.com>
2025-07-23 13:57:49 +0200

    Update install-ocp-sandbox-operator.sh

--- prerequisites/install-ocp-sandbox-operator.sh
+++ prerequisites/install-ocp-sandbox-operator.sh
@@ -58,7 +58,6 @@ metadata:
 spec:
   checkNodeEligibility: false
   enablePeerPods: false
-  kataConfigPoolSelector: null
   logLevel: info
 EOF
 
@@ -73,45 +72,3 @@ while kubectl get kataconfig example-kataconfig -n openshift-sandboxed-container
 done
 echo
 echo "done."
-
-# Waiting for next version of the operator, we currently need to add "io.kubernetes.cri-o.Devices"
-# to the allowed_annotations in the cri-o configuration to enable fuse support.
-# This is done by adding a MachineConfig to the worker nodes.
-# `ignition` syntax reference: https://coreos.github.io/ignition/configuration-v3_4/
-# echo "Creating MachineConfig to enable fuse when using kata"
-# kubectl apply -f - <<EOF
-# apiVersion: machineconfiguration.openshift.io/v1
-# kind: MachineConfig
-# metadata:
-#   labels:
-#     machineconfiguration.openshift.io/role: worker
-#   name: 50-kata-worker
-# spec:
-#   config:
-#     ignition:
-#       version: 3.4.0
-#     storage:
-#       files:
-#         - path: /etc/crio/crio.conf.d/50-kata
-#           append:
-#             source: data:,%20%20allowed_annotations%20%3D%20%5B%0A%20%20%20%20%22io.kubernetes.cri-o.Devices%22%2C%0A%20%20%5D
-# EOF
-
-# Or manually:
-#
-# # Get worker nodes names
-# oc get node -l node-role.kubernetes.io/worker -o json | jq '.items[].metadata.name'
-
-# # For each worker node:
-# NODE_NAME="ip-10-0-38-188.ec2.internal"
-# oc debug node/$NODE_NAME -- cat /host/etc/crio/crio.conf.d/50-kata
-
-# chroot /host
-# cat /etc/crio/crio.conf.d/50-kata
-# cat << EOF >>  /etc/crio/crio.conf.d/50-kata
-#   allowed_annotations = [
-#     "io.kubernetes.cri-o.Devices",
-#   ]
-# EOF
-# cat /etc/crio/crio.conf.d/50-kata
-# systemctl restart crio
